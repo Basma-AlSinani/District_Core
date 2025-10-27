4d30dc2a6dfa195964745cfd329121f52396c9c9
Basma Suliman <basmaalsinani97@gmail.com>
2025-10-27 14:58:36 +0400

    Add a generic repository that provides basic CRUD operations for any entity using Entity Framework.

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crime.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly CrimeDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(CrimeDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        //get all records
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        //get record by id
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        //add record
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        //update record
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        //delete record
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        //save changes
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Crime.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task SaveChangesAsync();
+        Task UpdateAsync(T entity);
+    }
+}
