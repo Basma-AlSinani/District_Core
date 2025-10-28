176abf0cc48d3170092124c4c4e51c68204b6b27
Basma Suliman <basmaalsinani97@gmail.com>
2025-10-28 09:21:40 +0400

    add CaseAssigneesRepo

---
+++ Repositories/ICaseAssigneesRepo.cs
@@ -0,0 +1,11 @@
+﻿using Crime.Models;
+
+namespace Crime.Repositories
+{
+    public interface ICaseAssigneesRepo
+    {
+        Task<bool> AssignUserToCaseAsync(int caseId, int userId, AssigneeRole role);
+        Task<IEnumerable<CaseAssignees>> GetAssigneesByCaseIdAsync(int caseId);
+        Task<bool> UpdateAssigneeStatusAsync(int caseAssigneeId, ProgreessStatus newStatus);
+    }
+}
