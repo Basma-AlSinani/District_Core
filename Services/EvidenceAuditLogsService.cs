76fc47ba47f21541980dc442718d1b491ca1bd4d
Basma Suliman <basmaalsinani97@gmail.com>
2025-10-28 10:33:36 +0400

    Add Service "EvidenceAuditLogs"

---
+++ Services/EvidenceAuditLogsService.cs
@@ -0,0 +1,38 @@
+﻿using Crime.Repositories;
+using Crime.Models;
+
+namespace Crime.Services
+{
+    public class EvidenceAuditLogsService : IEvidenceAuditLogsService
+    {
+        private readonly IEvidenceAuditLogsRepo _evidenceAuditLogsRepo;
+        public EvidenceAuditLogsService(IEvidenceAuditLogsRepo evidenceAuditLogsRepo)
+        {
+            _evidenceAuditLogsRepo = evidenceAuditLogsRepo;
+        }
+
+        public async Task<IEnumerable<EvidenceAuditLogs>> GetAllAsync()
+        {
+            return await _evidenceAuditLogsRepo.GetAllAsync();
+        }
+
+        public async Task<EvidenceAuditLogs> GetByIdAsync(int id)
+        {
+            var log = await _evidenceAuditLogsRepo.GetByIdAsync(id);
+            if (log == null)
+                throw new Exception("Audit log not found");
+            return log;
+        }
+
+        public async Task<IEnumerable<EvidenceAuditLogs>> GetLogsByEvidenceIdAsync(int evidenceId)
+        {
+            return await _evidenceAuditLogsRepo.GetLogsByEvidenceIdAsync(evidenceId);
+        }
+
+        public async Task AddAsync(EvidenceAuditLogs log)
+        {
+            await _evidenceAuditLogsRepo.AddAsync(log);
+        }
+
+    }
+}
---
+++ Services/IEvidenceAuditLogsService.cs
@@ -0,0 +1,12 @@
+﻿using Crime.Models;
+
+namespace Crime.Services
+{
+    public interface IEvidenceAuditLogsService
+    {
+        Task AddAsync(EvidenceAuditLogs log);
+        Task<IEnumerable<EvidenceAuditLogs>> GetAllAsync();
+        Task<EvidenceAuditLogs> GetByIdAsync(int id);
+        Task<IEnumerable<EvidenceAuditLogs>> GetLogsByEvidenceIdAsync(int evidenceId);
+    }
+}
