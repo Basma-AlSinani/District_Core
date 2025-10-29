b3acfcd0fb3dc5f30daed3fc9974585b6e435721
Basma Suliman <basmaalsinani97@gmail.com>
2025-10-29 15:54:36 +0400

    edit dtos

---
+++ DTOs/EvidenceDTOs.cs
@@ -0,0 +1,39 @@
+﻿using Crime.Models;
+
+namespace Crime.DTOs
+{
+    public class EvidenceDTOS
+    {
+        public class EvidenceCreateRequest
+        {
+            public int CaseId { get; set; }
+            public EvidenceType Type { get; set; }
+            public string? TextContent { get; set; } //text content for Text type
+            public IFormFile? File { get; set; } //file for Image type
+            public string? Remarks { get; set; }
+
+            public Users CreatedByUser { get; set; } //user who is adding the evidence
+        }
+
+        public class EvidenceUpdateTextRequest
+        {
+            public string? TextContent { get; set; }
+            public string? Remarks { get; set; }
+        }
+
+        public class EvidenceResponse
+        {
+            public int EvidenceId { get; set; }
+            public int CaseId { get; set; }
+            public string? TextContent { get; set; }
+            public string? FileUrl { get; set; }
+            public string? MimeType { get; set; }
+            public long? SizeBytes { get; set; }
+            public string Type { get; set; }
+            public string? Remarks { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public DateTime UpdatedAt { get; set; }
+            
+        }
+    }
+}
