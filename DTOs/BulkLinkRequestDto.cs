e3d5802d2a962c105c8cfdf82084721e9c97dced
Thuraiya <3eya151@gmail.com>
2025-10-30 08:41:19 +0400

    edit

--- DTOs/BulkLinkRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Crime.DTOs
-{
-    public class BulkLinkRequestDto
-    {
-        public int CaseId { get; set; }
-        public List<int> CrimeReportId { get; set; }
-        public int? PerformedBy { get; set; }
-    }
-}
