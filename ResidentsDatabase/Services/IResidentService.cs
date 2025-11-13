1b3e05fb976e1226a3e1d0646783a88f9806fa55
Basma Suliman <basmaalsinani97@gmail.com>
2025-11-13 11:16:49 +0400

    Add services for Resident

---
+++ ResidentsDatabase/Services/IResidentService.cs
@@ -0,0 +1,13 @@
+﻿using ResidentsDatabase.Models;
+
+namespace ResidentsDatabase.Services
+{
+    public interface IResidentService
+    {
+        Task AddResidentAsync(Resident resident);
+        Task DeleteResidentAsync(string residentId);
+        Task<IEnumerable<Resident>> GetAllResidentsAsync();
+        Task<Resident?> GetByResidentIdAsync(string residentId);
+        Task UpdateResidentAsync(Resident resident);
+    }
+}
---
+++ ResidentsDatabase/Services/ResidentService.cs
@@ -0,0 +1,36 @@
+﻿using ResidentsDatabase.Repositories;
+
+namespace ResidentsDatabase.Services
+{
+    public class ResidentService : IResidentService
+    {
+        private readonly IResidentRepo _residentRepo;
+        public ResidentService(IResidentRepo residentRepo)
+        {
+            _residentRepo = residentRepo;
+        }
+        public async Task AddResidentAsync(Models.Resident resident)
+        {
+            await _residentRepo.AddResidentAsync(resident);
+        }
+        public async Task<IEnumerable<Models.Resident>> GetAllResidentsAsync()
+        {
+            return await _residentRepo.GetAllResidentsAsync();
+        }
+
+        public async Task<Models.Resident?> GetByResidentIdAsync(string residentId)
+        {
+            return await _residentRepo.GetByResidentIdAsync(residentId);
+        }
+
+        public async Task UpdateResidentAsync(Models.Resident resident)
+        {
+            await _residentRepo.UpdateResidentAsync(resident);
+        }
+
+        public async Task DeleteResidentAsync(string residentId)
+        {
+            await _residentRepo.DeleteResidentAsync(residentId);
+        }
+    }
+}
