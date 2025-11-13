9cd5f44fff49f663f5733df899f6b503db0d1391
Basma Suliman <basmaalsinani97@gmail.com>
2025-11-13 11:09:50 +0400

    Add ResidentsDbContext

---
+++ ResidentsDatabase/ResidentsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+//using Microsoft.EntityFrameworkCore.Tools;
+
+namespace ResidentsDatabase
+{
+    public class ResidentsDbContext : DbContext
+    {
+        public ResidentsDbContext(DbContextOptions<ResidentsDbContext> options) : base(options) { }
+    }
+}
